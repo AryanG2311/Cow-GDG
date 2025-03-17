9aa29ab1f22793bf1d1a780fb63653fd1359447e
Arpan-gl <arpang754@gmail.com>
2025-03-17 22:51:34 +0530

    Add News page and Quiz page

---
+++ project/src/pages/News.jsx
@@ -0,0 +1,11 @@
+import { NewsGrid } from '../components/NewsGrid';
+
+function News() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <NewsGrid />
+    </div>
+  );
+}
+
+export default News;
